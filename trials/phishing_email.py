a3a5da9372d5faf54974e8e4b00fba8a10d0e316
The-Ancestor <ancestralbuddha007@gmail.com>
2026-02-04 21:11:20 +0200

    Create phishing_email.py

---
+++ trials/phishing_email.py
@@ -0,0 +1,8 @@
+Dear Customer,
+
+Your account has been SUSPENDED!!!
+Verify NOW to restore access immediately.
+
+http://paypal-secure-login-update.com/verify.zip
+
+Failure to act will result in permanent account closure.
